3f397d5aeca7e25c2de6f844175ad1a063d6108d
MEISON144 <113443625+MEISON144@users.noreply.github.com>
2023-03-19 14:44:09 +0500

    Windows

---
+++ js/save_button.js
@@ -0,0 +1,7 @@
+let save_button = document.querySelector('.save_button')
+
+function save(){
+    alert("Вы отправили данные оператору, скоро с вами свяжутся")
+}
+
+save_button.addEventListener('click', save)
